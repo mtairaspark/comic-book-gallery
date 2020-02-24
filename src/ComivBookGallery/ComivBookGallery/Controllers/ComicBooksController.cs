61933c1c7856b0b546eb8dc5e869a5547b94f6bf
Moises Taira <moises.taira@sparktechs.com>
2020-02-24 10:57:01 -0800

    Updated Detail action mehtod to use action result types

--- src/ComivBookGallery/ComivBookGallery/Controllers/ComicBooksController.cs
+++ src/ComivBookGallery/ComivBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,15 @@ namespace ComivBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic books controller";
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                //return new RedirectResult("/");
+                return Redirect("/");
+            }
+            return Content("Hello from the comic books controller");
+
         }
     }
 }
